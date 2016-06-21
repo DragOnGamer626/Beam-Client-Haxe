d8c9ba1e5d184bf6e93b116917953a8d7be6c120
Brandon Dulaney <draygera@gmail.com>
2016-06-21 11:35:15 -0500

    Started Working On Filling Out BeamAPI Class

--- src/pro/beam/api/BeamAPI.hx
+++ src/pro/beam/api/BeamAPI.hx
@@ -8,14 +8,37 @@ import sys.net.Socket;
 class BeamAPI
 {
 	public var uri(default, set) : String;
+	public var oauthToken(default, set) : String;
+	public var httpUserName(default, set) : String;
+	public var httpPassword(default, set) : String;
 	
 	public function new() 
 	{
 		
 	}
 	
-	function set_uri(uri) {
+	function set_uri(uri) 
+	{
 		return this.uri = uri;
 	}
 	
+	function set_oauthToken(oauthToken)
+	{
+		return this.oauthToken = oauthToken;
+	}
+	
+	function set_httpUserName(httpUserName)
+	{
+		return this.httpUserName = httpUserName;
+	}
+	
+	function set_httpPassword(httpPassword)
+	{
+		return this.httpPassword = httpPassword;
+	}
+	
+	public function buildJson() : Void
+	{
+		
+	}
 }
