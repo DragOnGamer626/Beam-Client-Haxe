f6ca4491bf3909fbf8c07eca3fb1a3177307feca
DragOnGamer626 <draygera@gmail.com>
2016-06-21 22:48:45 -0500

    Beam Channel Vars Start

--- src/pro/beam/api/resource/channel/BeamChannel.hx
+++ src/pro/beam/api/resource/channel/BeamChannel.hx
@@ -1,4 +1,5 @@
 package pro.beam.api.resource.channel;
+import haxe.Serializer;
 
 /**
  * ...
@@ -6,16 +7,44 @@ package pro.beam.api.resource.channel;
  */
 class BeamChannel
 {
-	public var id(default, set) : Int;
+	public var id : Int;
+	public var token : String;
+	public var online : Bool;
+	public var featured : Bool;
+	public var partnered : Bool;
+	public var transcodingEnabled : Bool;
+	public var suspended : Bool;
+	public var interactive : Bool;
+	public var hasVod : Bool;
+	public var name : String;
+	public var audience : AudienceRating;
+	public var streamKey : String;
+	public var viewersTotal : Int;
+	public var viewersCurrent : Int;
+	public var numFollowers : Int;
+	public var numSubscribers : Int;
+	public var description : String;
+	public var typeId : Int;
+	public var createdAt : Date;
+	public var updateAt : Date;
+	public var userId : Int;
+	public var coverId : Int;
+	public var thumbnailId : Int;
+	public var badgeId : Int;
+	public var thumbnail : BeamResource;
+	public var cover : BeamResource;
+	public var badge : BeamResource;
+	public var type : Type;
+	public var preferences : Map<String, Dynamic>;
+	public var messageCache : Array<CachedMessage>;
 	
-	public function new() 
-	{
-		
+	public function new() {
+		var ser : Serializer = new Serializer();
 	}
 	
-	function set_id(id : Int) : Int
-	{
-		return this.id = id;
+	public static enum AudienceRating {
+		FAMILY("family");
+		TEEN("teen");
+		ADULT("18+");
 	}
-	
 }
