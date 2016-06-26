c246813c935e293c5fb22b1cbd2f8d4a76191382
DragOnGamer626 <draygera@gmail.com>
2016-06-25 20:03:36 -0500

    More Checker Work

--- src/pro/beam/api/futures/checkers/Channels.hx
+++ src/pro/beam/api/futures/checkers/Channels.hx
@@ -1,4 +1,6 @@
 package pro.beam.api.futures.checkers;
+import pro.beam.api.futures.AbstractChecker;
+import pro.beam.api.futures.checkers.Channels.StatusChecker;
 
 /**
  * ...
@@ -6,10 +8,25 @@ package pro.beam.api.futures.checkers;
  */
 class Channels
 {
-
-	public function new() 
+	public static function init()
 	{
-		
+		StatusChecker.initialize();
+	}
+}
+
+class StatusChecker extends AbstractChecker
+{
+	static var MISSING_PERMISSION_CODE(default, never) : Int = 403;
+	
+	function new()
+	{
+		this.map = [
+			new Pair<Int, String>(MISSING_PERMISSION_CODE, "Missing Permission Code Error Thrown")
+		];
 	}
 	
+	public static function initialize() : StatusChecker
+	{
+		return new StatusChecker();
+	}
 }
--- src/pro/beam/api/futures/checkers/Users.hx
+++ src/pro/beam/api/futures/checkers/Users.hx
@@ -1,7 +1,8 @@
 package pro.beam.api.futures.checkers;
 import pro.beam.api.futures.AbstractChecker;
+import pro.beam.api.futures.checkers.Users.ForgotPasswordChecker;
+import pro.beam.api.futures.checkers.Users.ResetPasswordChecker;
 import pro.beam.api.futures.checkers.Users.TwoFactorFutureChecker;
-import tink.core.Outcome;
 
 /**
  * ...
@@ -11,7 +12,12 @@ import tink.core.*;
 
 class Users
 {
-	
+	public static function init() : Void
+	{
+		TwoFactorFutureChecker.initialize();
+		ForgotPasswordChecker.initialize();
+		ResetPasswordChecker.initialize();
+	}
 }
 
 class TwoFactorFutureChecker extends AbstractChecker
@@ -19,12 +25,17 @@ class TwoFactorFutureChecker extends AbstractChecker
 	static var WRONG_PASSWORD_RESPONSE(default, never) : Int = 401;
 	static var TWOFACTOR_WRONG_CODE_RESPONSE(default, never) : Int = 409;
 	
-	public function new() 
+	function new()
 	{
-		map = [
+		this.map = [
 			new Pair<Int, String>(WRONG_PASSWORD_RESPONSE, "Wrong Password Error Thrown"),
 			new Pair<Int, String>(TWOFACTOR_WRONG_CODE_RESPONSE, "Two Factor Wrong Code Error Thrown")
 		];
+	}
+	
+	public static function initialize() : TwoFactorFutureChecker
+	{
+		return new TwoFactorFutureChecker();
 	}
 }
 
@@ -33,12 +44,17 @@ class ForgotPasswordChecker extends AbstractChecker
 	static var MISSING_USER_CODE = 404;
 	static var TOO_MANY_REQUESTS = 429;
 	
-	public function new()
+	function new()
 	{
-		map = [
+		this.map = [
 			new Pair<Int, String>(MISSING_USER_CODE, "Missing User Code Error Thrown"),
 			new Pair<Int, String>(TOO_MANY_REQUESTS, "Too Many Requests Error Thrown")
 		];
+	}
+	
+	public static function initialize() : ForgotPasswordChecker
+	{
+		return new ForgotPasswordChecker();
 	}
 }
 
@@ -47,12 +63,17 @@ class ResetPasswordChecker extends AbstractChecker
 	static var MISSING_USER_CODE = 404;
 	static var BAD_REQUEST_CODE = 400;
 	
-	public function new()
+	function new()
 	{
-		map = [
+		this.map = [
 			new Pair<Int, String>(MISSING_USER_CODE, "Missing User Code Error Thrown"),
 			new Pair<Int, String>(BAD_REQUEST_CODE, "Bad Request Code Error Thrown")
 		];
+	}
+	
+	public static function initialize() : ResetPasswordChecker
+	{
+		return new ResetPasswordChecker();
 	}
 }
 
